0e8483cb81bdf0599be97d6b74844dcbde054389
wwnp <iamtodaysad@gmail.com>
2021-10-31 22:01:48 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import Layout from "./HOC/Layout/Layout";
+
 function App() {
   return (
     <div className="App">
+      <Layout>
+        <div style={{border:'1px solid black',width:400,textAlign:'center'}}>
+          <h1>Layout</h1>
+        </div>
+      </Layout>
     </div>
   );
 }
 
-export default App;
+export default App 
